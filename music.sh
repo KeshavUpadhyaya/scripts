0b64f085e0fc57c40f0924a4b1724ad2105384fa
KeshavUpadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2024-11-30 11:57:58 +0100

    Add files via upload

---
+++ music.sh
@@ -0,0 +1,3 @@
+yt-dlp --cookies-from-browser firefox -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" "https://music.youtube.com/watch?v=8Xv_Hg8o1fw"
+
+yt-dlp -f "bestaudio" -o "%(playlist_title)s/%(title)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 0 --cookies-from-browser firefox https://music.youtube.com/playlist\?list\=LM
