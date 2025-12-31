5300118436433d3a724034920f85a44fa16ec4a1
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2025-12-31 20:15:49 +0100

    Add yt-dlp commands for audio extraction
    Added new yt-dlp command for m4a format with metadata embedding.

--- music.sh
+++ music.sh
@@ -1,3 +1,10 @@
 yt-dlp --cookies-from-browser firefox -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" "https://music.youtube.com/watch?v=8Xv_Hg8o1fw"
 
 yt-dlp -f "bestaudio" -o "%(playlist_title)s/%(title)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 0 --cookies-from-browser firefox https://music.youtube.com/playlist\?list\=LM
+
+yt-dlp -f bestaudio \
+--extract-audio \
+--audio-format m4a \
+--embed-metadata \
+-o "%(playlist_title)s/%(title)s.%(ext)s" \
+--cookies-from-browser firefox https://www.youtube.com/playlist?list=PL
