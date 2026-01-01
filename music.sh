865fe26ec9a5e2bb5bb08d7ae9632d79068f4573
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2026-01-01 21:27:01 +0100

    Enhance music.sh for playlist and single item downloads

--- music.sh
+++ music.sh
@@ -1,7 +1,4 @@
-yt-dlp --cookies-from-browser firefox -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 -o "%(title)s.%(ext)s" "https://music.youtube.com/watch?v=8Xv_Hg8o1fw"
-
-yt-dlp -f "bestaudio" -o "%(playlist_title)s/%(title)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 0 --cookies-from-browser firefox https://music.youtube.com/playlist\?list\=LM
-
+# playlist
 yt-dlp -f bestaudio \
 --extract-audio \
 --audio-format m4a \
@@ -9,3 +6,13 @@ yt-dlp -f bestaudio \
 --embed-thumbnail \
 -o "%(playlist_title)s/%(title)s.%(ext)s" \
 --cookies-from-browser firefox https://www.youtube.com/playlist?list=PL
+
+# single item
+yt-dlp -f bestaudio \
+  --extract-audio \
+  --audio-format m4a \
+  --embed-metadata \
+  --embed-thumbnail \
+  --cookies-from-browser firefox \
+  -o "%(title)s.%(ext)s" \
+  https://www.youtube.com/watch?v=VIDEO_ID
